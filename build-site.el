c35f741f99cfe942773de1d6e2edb3cf4d8e6495
swaminsane <swami13vivekanand@gmail.com>
2026-04-06 20:22:54 +0530

    local theme

--- build-site.el
+++ build-site.el
@@ -22,8 +22,7 @@
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
 ;;      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
-      org-html-head "<link rel=\"stylesheet\" href=\"https://latex.vercel.app/style.css\" />")
-
+      org-html-head "<link rel=\"stylesheet\" href=\"./style.css\" />")
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
